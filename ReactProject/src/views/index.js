e1edb7bcebd579c0939a1f8fdff0feca46fe4031
Mona Salari <158235172+Monaslr@users.noreply.github.com>
2024-10-27 08:31:04 -0600

    Add files via upload

---
+++ ReactProject/src/views/index.js
@@ -0,0 +1,7 @@
+import ProjectCreate from "./ProjectCreate";
+import TargetView from "./TargetView";
+import About from "./About";
+import Home from "./Home";
+
+
+export default {ProjectCreate, TargetView, Home, About};
